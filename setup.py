ce987a7ca138fc1544ea002361d5167a2fc24a3e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-06-12 14:52:33 +0200

    distutils setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+
+from distutils.core import setup
+import time
+
+
+setup(
+	name = 'better_exchook',
+	version = time.strftime("1.%Y%m%d.%H%M%S", time.gmtime()),
+	packages = ['better_exchook'],
+	package_dir = {'better_exchook': ''},
+	description = 'nice Python exception hook replacement',
+	author = 'Albert Zeyer',
+	author_email = 'albzey@gmail.com',
+	url = 'https://github.com/albertz/py_better_exchook',
+	license = '2-clause BSD license',
+	long_description = open('README.rst').read(),
+	# https://pypi.python.org/pypi?%3Aaction=list_classifiers
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Education',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX',
+		'Operating System :: Unix',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		]
+)
+
